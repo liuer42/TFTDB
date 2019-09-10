fce9fb6d7b9c603dbee3e1acbbfe44c0279724f9
Eric Liu <eliu@my.wctc.edu>
2019-09-09 20:41:55 -0500

    update servlet

--- src/Servlet.java
+++ src/Servlet.java
@@ -5,23 +5,23 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "FirstServlet", urlPatterns = "/hello")
+@WebServlet(name = "Servlet", urlPatterns = "/hello")
 
-public class FirstServlet extends HttpServlet {
+public class Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String username = request.getParameter("Username");
+        /*String username = request.getParameter("Username");
         String password = request.getParameter("Password");
 
         String msg = "<html><body> Thanks, " + username  + ", your password is " + password +
                 "</body></html>";
-        response.getWriter().print(msg);
+        response.getWriter().print(msg);*/
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String p = request.getParameter("param1");
+        /*String p = request.getParameter("param1");
 
 
         String msg = "<html><body>" + p + "</body></html>";
-        response.getWriter().print(msg);
+        response.getWriter().print(msg);*/
     }
 }
