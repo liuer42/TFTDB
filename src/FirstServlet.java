180a9fc7eaaa3ceb40866f419fe3d21f549df29d
Eric Liu <eliu@my.wctc.edu>
2019-09-09 20:20:37 -0500

    initial

---
+++ src/FirstServlet.java
@@ -0,0 +1,27 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "FirstServlet", urlPatterns = "/hello")
+
+public class FirstServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String username = request.getParameter("Username");
+        String password = request.getParameter("Password");
+
+        String msg = "<html><body> Thanks, " + username  + ", your password is " + password +
+                "</body></html>";
+        response.getWriter().print(msg);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String p = request.getParameter("param1");
+
+
+        String msg = "<html><body>" + p + "</body></html>";
+        response.getWriter().print(msg);
+    }
+}
