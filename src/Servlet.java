8b1cceeb88c5ecf5d77f56142bf7341bfd833d2d
Eric Liu <eliu@my.wctc.edu>
2019-09-09 21:13:40 -0500

    resultset commit

--- src/Servlet.java
+++ src/Servlet.java
@@ -4,6 +4,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 @WebServlet(name = "Servlet", urlPatterns = "/hello")
 
@@ -15,6 +19,25 @@ public class Servlet extends HttpServlet {
         String msg = "<html><body> Thanks, " + username  + ", your password is " + password +
                 "</body></html>";
         response.getWriter().print(msg);*/
+        String absPath = getServletContext().getRealPath("/WEB-INF/../lib/tftdb");
+
+        try {
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+            Connection conn = DriverManager.getConnection("jdbc:derby:" + absPath, "eric", "eric");
+            Statement stmt = conn.createStatement();
+
+            ResultSet rset = stmt.executeQuery("SELECT * FROM Classes");
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+
+
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
